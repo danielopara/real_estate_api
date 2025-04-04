9cc1ff06baa2265ec69e9fca44c7a579bfef33ee
DanielOpara <oparadaniv@gmail.com>
2025-04-04 22:10:14 +0100

    get auth user

--- api/User/urls.py
+++ api/User/urls.py
@@ -1,6 +1,7 @@
-from api.user.views import create_user
+from api.user.views import create_user, get_user
 from django.urls import path
 
 urlpatterns = [
-    path('create_user', create_user, name='create_user')
+    path('create_user', create_user, name='create_user'),
+    path('get_user', get_user, name='get_auth_user')
 ]
--- api/User/views.py
+++ api/User/views.py
@@ -1,5 +1,6 @@
 from api.user.service import UserService
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['POST'])
@@ -9,3 +10,8 @@ def create_user(request):
     """
     user_service = UserService()
     return user_service.create_user(request)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    return UserService().get_auth_user(request)
