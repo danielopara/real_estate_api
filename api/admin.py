0580ed18ce7169e382c16ecb77a1d6f8417327a4
DanielOpara <oparadaniv@gmail.com>
2025-04-06 20:13:24 +0100

    adding property to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
-from api.models import Property, UserAccount
+from api.models import Apartment, Property, UserAccount
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(UserAccount)
 admin.site.register(Property)
+admin.site.register(Apartment)
